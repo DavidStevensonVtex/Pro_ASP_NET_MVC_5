10293c20c354cb34c7d8494de654ef9c36c4e7d6
David Stevenson <drstevenson1958@yahoo.com>
2023-07-10 17:23:04 -0400

    Handle Post Back of Form Data - GuestResponse

--- ch02/ch02_ProASPNETMVC5/PartyInvites/Controllers/HomeController.cs
+++ ch02/ch02_ProASPNETMVC5/PartyInvites/Controllers/HomeController.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
-    public class HomeController : Controller
+	public class HomeController : Controller
     {
         public ViewResult Index()
         {
@@ -15,9 +13,17 @@ namespace PartyInvites.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
 		{
             return View();
+		}
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+		{
+            // TODO: Email response to the party organizer
+            return View("Thanks", guestResponse);
 		}
     }
 }
