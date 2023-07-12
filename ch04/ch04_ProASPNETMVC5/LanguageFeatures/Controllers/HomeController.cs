e29c38e1a117e3cbeaa29acf79df8e191c309a2e
David Stevenson <drstevenson1958@yahoo.com>
2023-07-12 10:54:41 -0400

    Listing 4-4. Consuming a Property in the HomeController.cs File

--- ch04/ch04_ProASPNETMVC5/LanguageFeatures/Controllers/HomeController.cs
+++ ch04/ch04_ProASPNETMVC5/LanguageFeatures/Controllers/HomeController.cs
@@ -1,16 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers
 {
-    public class HomeController : Controller
+	public class HomeController : Controller
     {
         public string Index()
         {
             return "Navigate to a URL to show an example";
         }
+
+        public ViewResult AutoProperty()
+		{
+            // createa new Product object
+            Product myProduct = new Product();
+
+            // set the property value
+            myProduct.Name = "Kayak";
+
+            // get the property
+            string productName = myProduct.Name;
+
+            // generate the view
+            return View("Result", (object) $"Product Name: {productName}");
+		}
     }
 }
