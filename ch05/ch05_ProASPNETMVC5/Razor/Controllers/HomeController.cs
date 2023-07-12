39dac639e8b0146fb8a7bdf622421244643a3242
David Stevenson <drstevenson1958@yahoo.com>
2023-07-12 15:22:28 -0400

    Listing 5-3. The Contents of the Index.cshtml File

--- ch05/ch05_ProASPNETMVC5/Razor/Controllers/HomeController.cs
+++ ch05/ch05_ProASPNETMVC5/Razor/Controllers/HomeController.cs
@@ -1,9 +1,9 @@
-﻿using Razor.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Razor.Models;
 
 namespace Razor.Controllers
 {
