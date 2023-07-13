60ab73428f4351cc2f3b82b6a23f058b3ade6927
David Stevenson <drstevenson1958@yahoo.com>
2023-07-13 09:34:23 -0400

    Listing 6-27. Adding the Test Methods to the UnitTest1.cs File

---
+++ ch06/ch06_ProASPNETMVC5/EssentialTools.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTools.Models;
+
+namespace EssentialTools.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		private IDiscountHelper getTestObject()
+		{
+			return new MinimumDiscountHelper();
+		}
+		[TestMethod]
+		public void TestMethod1()
+		{
+			// Arrange
+			IDiscountHelper target = getTestObject();
+			decimal total = 200;
+
+			// Act
+			var discountedTotal = target.ApplyDiscount(total);
+
+			// Assert
+			Assert.AreEqual(total * 0.9M, discountedTotal);
+		}
+	}
+}
