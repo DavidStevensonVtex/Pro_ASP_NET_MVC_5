6915a7df0f88a5552f26f1070d565eb0f72cd499
David Stevenson <drstevenson1958@yahoo.com>
2023-07-13 09:48:19 -0400

    Listing 6-28. Defining the Remaining Tests in the UnitTest1.cs File

--- ch06/ch06_ProASPNETMVC5/EssentialTools.Tests/UnitTest1.cs
+++ ch06/ch06_ProASPNETMVC5/EssentialTools.Tests/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace EssentialTools.Tests
 			return new MinimumDiscountHelper();
 		}
 		[TestMethod]
-		public void TestMethod1()
+		public void Discount_Above_100()
 		{
 			// Arrange
 			IDiscountHelper target = getTestObject();
@@ -23,6 +23,49 @@ namespace EssentialTools.Tests
 
 			// Assert
 			Assert.AreEqual(total * 0.9M, discountedTotal);
+		}
+
+		[TestMethod]
+		public void Discount_Between_10_And_100()
+		{
+			// Arrange
+			IDiscountHelper target = getTestObject();
+
+			// Act
+			decimal TenDollarDiscount = target.ApplyDiscount(10);
+			decimal HundredDollarDiscount = target.ApplyDiscount(100);
+			decimal FiftyDollarDiscount = target.ApplyDiscount(50);
+
+			// Assert
+			Assert.AreEqual(5, TenDollarDiscount, "$10 discount is wrong");
+			Assert.AreEqual(95, HundredDollarDiscount, "$100 discount is wrong");
+			Assert.AreEqual(45, FiftyDollarDiscount, "$50 discount is wrong");
+		}
+
+		[TestMethod]
+		public void Discount_Less_Than_10()
+		{
+			// Arrange
+			IDiscountHelper target = getTestObject();
+
+			// Act
+			decimal discount5 = target.ApplyDiscount(5);
+			decimal discount0 = target.ApplyDiscount(0);
+
+			// Assert
+			Assert.AreEqual(5, discount5);
+			Assert.AreEqual(0, discount0);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void Discount_Negative_Total()
+		{
+			// Arrange
+			IDiscountHelper target = getTestObject();
+
+			// Act
+			target.ApplyDiscount(-1);
 		}
 	}
 }
