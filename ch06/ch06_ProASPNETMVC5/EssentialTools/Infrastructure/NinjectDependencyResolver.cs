82e2f8094f880793c2bf72e076ea7655ab5e3965
David Stevenson <drstevenson1958@yahoo.com>
2023-07-13 09:24:06 -0400

    Listing 6-25. Using the Request Scope in the NinjectDependencyResolver.cs File

--- ch06/ch06_ProASPNETMVC5/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
+++ ch06/ch06_ProASPNETMVC5/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using EssentialTools.Models;
 using Ninject;
+using Ninject.Web.Common;
 
 namespace EssentialTools.Infrastructure
 {
@@ -30,7 +29,7 @@ namespace EssentialTools.Infrastructure
 
 		private void AddBindings()
 		{
-			kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+			kernel.Bind<IValueCalculator>().To<LinqValueCalculator>().InRequestScope();
 			kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>()
 				.WithConstructorArgument("discountParam", 50M);
 			kernel.Bind<IDiscountHelper>().To<FlexibleDiscountHelper>()
