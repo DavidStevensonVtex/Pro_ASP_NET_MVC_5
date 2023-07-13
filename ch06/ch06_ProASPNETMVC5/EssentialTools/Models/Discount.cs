36197253b52c8d7248a1935ef9afd90fda913115
David Stevenson <drstevenson1958@yahoo.com>
2023-07-13 08:44:43 -0400

    Listing 6-14. The Contents of the Discount.cs and IDiscountHelper.cs Files

---
+++ ch06/ch06_ProASPNETMVC5/EssentialTools/Models/Discount.cs
@@ -0,0 +1,10 @@
+﻿namespace EssentialTools.Models
+{
+	public class Discount : IDiscountHelper
+	{
+		public decimal ApplyDiscount(decimal totalParam)
+		{
+			return (totalParam - (10m / 100m * totalParam));
+		}
+	}
+}
