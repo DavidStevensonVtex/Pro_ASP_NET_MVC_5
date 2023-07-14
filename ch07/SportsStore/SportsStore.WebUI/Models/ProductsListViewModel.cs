0f479a1673b8bd17aad4f10ebcadccaaa454e579
David Stevenson <drstevenson1958@yahoo.com>
2023-07-14 09:55:42 -0400

    Listing 7-20. The Contents of the ProductsListViewModel.cs

---
+++ ch07/SportsStore/SportsStore.WebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Models
+{
+	public class ProductsListViewModel
+	{
+		public IQueryable<Product> Products { get; set; }
+		public PagingInfo PagingInfo { get; set; }
+	}
+}
