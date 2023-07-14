fbe71a9d687743231873d2d348da99253916b9b3
David Stevenson <drstevenson1958@yahoo.com>
2023-07-14 08:41:13 -0400

    Listing 7-12. The Content of the EFDbContext.cs File

--- ch07/SportsStore/SportsStore.Domain/Concrete/EFDbContext.cs
+++ ch07/SportsStore/SportsStore.Domain/Concrete/EFDbContext.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.Domain.Concrete
 {
 	public class EFDbContext
 	{
+		public DbSet<Product> Products { get; set; }
 	}
 }
