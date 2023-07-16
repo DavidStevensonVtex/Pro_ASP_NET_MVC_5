fe405ea6ec674c36d8889131c3fea28e232c4378
David Stevenson <drstevenson1958@yahoo.com>
2023-07-16 17:46:24 -0400

    Listing 10-5. Updating the Menu Action Method in the NavController.cs File

--- ch10/SportsStore/SportsStore.WebUI/Controllers/NavController.cs
+++ ch10/SportsStore/SportsStore.WebUI/Controllers/NavController.cs
@@ -14,7 +14,7 @@ namespace SportsStore.WebUI.Controllers
             repository = repo;
 		}
 
-        public PartialViewResult Menu(string category = null)
+        public PartialViewResult Menu(string category = null, bool horizontalLayout = false)
         {
             ViewBag.SelectedCategory = category;
 
@@ -24,7 +24,9 @@ namespace SportsStore.WebUI.Controllers
                 .OrderBy(x => x)
                 .ToList();
 
-            return PartialView(categories);
+            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
+
+            return PartialView(viewName, categories);
         }
     }
 }
