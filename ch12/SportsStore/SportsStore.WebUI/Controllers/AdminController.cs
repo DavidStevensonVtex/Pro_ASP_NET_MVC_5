3f34061708fd73212c69fcdd7694029baea10b73
David Stevenson <drstevenson1958@yahoo.com>
2023-07-17 14:41:24 -0400

    Chapter 12: SportsStore: Security & Finishing Touches

---
+++ ch12/SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Controllers
+{
+	public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+		{
+            repository = repo;
+		}
+
+        public ViewResult Index()
+        {
+            return View(repository.Products.ToList());
+        }
+
+        public ViewResult Edit(int productId)
+		{
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            return View(product);
+		}
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+		{
+            if (ModelState.IsValid)
+			{
+                repository.SaveProduct(product);
+                TempData["message"] = $"{product.Name} has been saved";
+                return RedirectToAction("Index");
+			}
+            else
+			{
+                // there is something wrong with the data values
+                return View(product);
+			}
+		}
+
+        public ViewResult Create()
+		{
+            return View("Edit", new Product());
+		}
+
+        [HttpPost]
+        public ActionResult Delete(int productId)
+		{
+            Product deletedProduct = repository.DeleteProduct(productId);
+            if (deletedProduct != null)
+            {
+                TempData["message"] = $"{deletedProduct.Name} was deleted";
+			}
+
+            return RedirectToAction("Index");
+		}
+    }
+}
