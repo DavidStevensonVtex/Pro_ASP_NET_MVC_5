0477b2cd89e1633e7511e9353beca3aed1e4e2a2
David Stevenson <drstevenson1958@yahoo.com>
2023-07-17 09:54:16 -0400

    Listing 11-1. The Contents of the AdminController.cs File

---
+++ ch11/SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+	public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+		{
+            repository = repo;
+		}
+
+        public ActionResult Index()
+        {
+            return View(repository.Products.ToList());
+        }
+    }
+}
