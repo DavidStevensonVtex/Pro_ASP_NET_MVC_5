7a3ed734f175bfcbd9a3919cf1621945e6a23d5e
David Stevenson <drstevenson1958@yahoo.com>
2023-07-18 08:57:54 -0400

    Listing 14-1. The Contents of the HomeController.cs File

--- ch14/DebuggingDemo/Controllers/HomeController.cs
+++ ch14/DebuggingDemo/Controllers/HomeController.cs
@@ -10,19 +10,11 @@ namespace DebuggingDemo.Controllers
 	{
 		public ActionResult Index()
 		{
-			return View();
-		}
+			int firstVal = 10;
+			int secondVal = 5;
+			int result = firstVal / secondVal;
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
+			ViewBag.Message = "Welcome to ASP.NET MVC!";
 
 			return View();
 		}
