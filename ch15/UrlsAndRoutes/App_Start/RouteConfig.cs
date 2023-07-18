3c42f4bfbadad91bb924dbf95760e9ecb68303b7
David Stevenson <drstevenson1958@yahoo.com>
2023-07-18 13:45:53 -0400

    Listing 15-34. Applying a Custom Constraint in a Route in the RouteConfig.cs File

--- ch15/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ ch15/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Routing.Constraints;
 using System.Web.Routing;
+using UrlsAndRoutes.Infrastructure;
 
 namespace UrlsAndRoutes
 {
@@ -12,6 +13,12 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+
+            routes.MapRoute("ChromeRoute", "{*catchall}",
+                 new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                 new { customConstraint = new UserAgentConstraint("Chrome") },
+                 new[] { "UrlsAndRoutes.AdditionalControllers" });
+
             Route myRoute = routes.MapRoute("MyRoute", "{controller}/{action}/{id}/{*catchall}",
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional },
                 new { 
