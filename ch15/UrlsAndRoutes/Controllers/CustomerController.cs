6902c7f5b16e5055639da976ebee4d7c09e3679b
David Stevenson <drstevenson1958@yahoo.com>
2023-07-18 13:53:08 -0400

    Listing 15-36. Applying the Route Attribute in the CustomerController.cs File

--- ch15/UrlsAndRoutes/Controllers/CustomerController.cs
+++ ch15/UrlsAndRoutes/Controllers/CustomerController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace UrlsAndRoutes.Controllers
 {
-    public class CustomerController : Controller
+	public class CustomerController : Controller
     {
+        [Route("Test")]
         public ActionResult Index()
         {
             ViewBag.Controller = "Customer";
