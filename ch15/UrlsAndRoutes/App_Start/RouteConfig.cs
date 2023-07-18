f443ec4bae4987c9ff0806f2f24494eb332c2fa2
David Stevenson <drstevenson1958@yahoo.com>
2023-07-18 11:16:57 -0400

    Listing 15-9. Providing a Default Value in the RouteConfig.cs File

--- ch15/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ ch15/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -11,7 +11,8 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.MapRoute("MyRoute", "{controller}/{action}");
+            routes.MapRoute("MyRoute", "{controller}/{action}",
+                new { controller = "Home", action = "Index" } );
         }
     }
 }
