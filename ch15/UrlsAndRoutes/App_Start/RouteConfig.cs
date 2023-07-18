6bfc099ec8c949c1b9c5bcb2b5e58c43aa33199a
David Stevenson <drstevenson1958@yahoo.com>
2023-07-18 13:28:22 -0400

    Listing 15-31. Using a Build-In Type/Value Constraint in the RouteConfig.cs File

--- ch15/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ ch15/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc.Routing.Constraints;
 using System.Web.Routing;
 
 namespace UrlsAndRoutes
@@ -13,7 +14,12 @@ namespace UrlsAndRoutes
         {
             Route myRoute = routes.MapRoute("MyRoute", "{controller}/{action}/{id}/{*catchall}",
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-                new { controller = "^H.*", action = "^Index$|^About$", httpMethod = new HttpMethodConstraint("GET") },
+                new { 
+                    controller = "^H.*", 
+                    action = "^Index$|^About$", 
+                    httpMethod = new HttpMethodConstraint("GET"), 
+                    id = new RangeRouteConstraint(10, 20)
+                },
                 new[] { "UrlsAndRoutes.Controllers" });
         }
     }
