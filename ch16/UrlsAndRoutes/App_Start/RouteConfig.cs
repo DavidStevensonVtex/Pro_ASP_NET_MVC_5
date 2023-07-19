33071e8e77b018a4efeaf90ee278df10ff20433b
David Stevenson <drstevenson1958@yahoo.com>
2023-07-19 10:53:55 -0400

    Chapter 16: Advanced Routing Features
    Generating Outgoing URLs which can be embedded in views.

---
+++ ch16/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Routing.Constraints;
+using System.Web.Routing;
+using UrlsAndRoutes.Infrastructure;
+
+namespace UrlsAndRoutes
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute("Default", "{controller}/{action}/{id}",
+                new { controller = "Home", action = "index", id = UrlParameter.Optional },
+                new [] { "UrlsAndRoutes.Controllers" });
+        }
+    }
+}
