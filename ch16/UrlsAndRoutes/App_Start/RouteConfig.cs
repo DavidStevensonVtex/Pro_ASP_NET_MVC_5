c1e0f00110845c069f23cf5c1c0b79ed8adaa83f
David Stevenson <drstevenson1958@yahoo.com>
2023-07-19 14:50:42 -0400

    Listing 16-32. Enabling Route Evalluation Before File-Checking in the RouteConfig.cs File

--- ch16/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ ch16/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -8,6 +8,8 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.RouteExistingFiles = true;
+
             routes.MapMvcAttributeRoutes();
 
             routes.Add(new Route("SayHello", new CustomRouteHandler()));
