dfcf0e5c7b53c839d2bc775f9c803fc8571416b6
David Stevenson <drstevenson1958@yahoo.com>
2023-07-19 10:57:19 -0400

    Listing 16-1. Simplifying the Example Routes in the RouteConfig.cs File

--- ch16/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ ch16/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -15,9 +15,12 @@ namespace UrlsAndRoutes
         {
             routes.MapMvcAttributeRoutes();
 
-            routes.MapRoute("Default", "{controller}/{action}/{id}",
-                new { controller = "Home", action = "index", id = UrlParameter.Optional },
-                new [] { "UrlsAndRoutes.Controllers" });
+            routes.MapRoute("MyRoute", "{controller}/{action}/{id}",
+                new { 
+                    controller = "Home", 
+                    action = "Index", 
+                    id = UrlParameter.Optional 
+                });
         }
     }
 }
