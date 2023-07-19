227510d7870a9f1e8744f657638114cf8c6d205f
David Stevenson <drstevenson1958@yahoo.com>
2023-07-19 11:12:14 -0400

    Delete the HomeController.cs in the AdditionalControllers folder / namespace.

--- ch16/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ ch16/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -15,6 +15,9 @@ namespace UrlsAndRoutes
         {
             routes.MapMvcAttributeRoutes();
 
+            routes.MapRoute("NewRoute", "App/Do{action}",
+                new { controller = "Home" });
+
             routes.MapRoute("MyRoute", "{controller}/{action}/{id}",
                 new { 
                     controller = "Home", 
