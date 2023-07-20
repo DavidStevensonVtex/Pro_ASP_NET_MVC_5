15a8e9506569b3d9672e6726a172fa23106966dd
David Stevenson <drstevenson1958@yahoo.com>
2023-07-20 13:25:24 -0400

    Unit Test: View Model Objects

--- ch17/ControllersAndActions.Tests/ActionTests.cs
+++ ch17/ControllersAndActions.Tests/ActionTests.cs
@@ -1,7 +1,7 @@
-﻿using ControllersAndActions.Controllers;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
 using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ControllersAndActions.Controllers;
 
 namespace ControllersAndActions.Tests
 {
@@ -18,7 +18,21 @@ namespace ControllersAndActions.Tests
 			ViewResult result = target.Index();
 
 			// Assert - check the result
-			Assert.AreEqual("Homepage", result.ViewName);
+			Assert.AreEqual("", result.ViewName);
+		}
+
+		[TestMethod]
+		public void ViewSelectionTest()
+		{
+			// Arrange - create the controller
+			ExampleController target = new ExampleController();
+
+			// Act - call the action method
+			ViewResult result = target.Index();
+
+			// Assert - check the result
+			Assert.AreEqual("", result.ViewName);
+			Assert.IsInstanceOfType(result.ViewData.Model, typeof(System.DateTime));
 		}
 	}
 }
