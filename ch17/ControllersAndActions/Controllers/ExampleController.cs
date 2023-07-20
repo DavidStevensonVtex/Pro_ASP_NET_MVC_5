907e07115bb10afbf65549eb77381e1442e17f69
David Stevenson <drstevenson1958@yahoo.com>
2023-07-20 13:58:19 -0400

    Preserving Data Across a Redirection

--- ch17/ControllersAndActions/Controllers/ExampleController.cs
+++ ch17/ControllersAndActions/Controllers/ExampleController.cs
@@ -7,13 +7,16 @@ namespace ControllersAndActions.Controllers
     {
         public ViewResult Index()
         {
-            ViewBag.Message = "Hello";
-            ViewBag.Date = DateTime.Now;
+            ViewBag.Message = TempData["Message"];
+            ViewBag.Date = TempData["Date"];
             return View();
         }
 
         public RedirectToRouteResult Redirect2()
 		{
+            // Preserving Data Across a Redirection
+            TempData["Message"] = "Hello";
+            TempData["Date"] = DateTime.Now;
             return RedirectToAction("Index");
 		}
     }
