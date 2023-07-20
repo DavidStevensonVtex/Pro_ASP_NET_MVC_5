2120145944a49b9994d964be7389a85893102e22
David Stevenson <drstevenson1958@yahoo.com>
2023-07-20 13:17:23 -0400

    Listing 17-15. Specifying a View Model Object in the ExampleController.cs File

--- ch17/ControllersAndActions/Controllers/ExampleController.cs
+++ ch17/ControllersAndActions/Controllers/ExampleController.cs
@@ -1,16 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ControllersAndActions.Controllers
 {
-    public class ExampleController : Controller
+	public class ExampleController : Controller
     {
         public ViewResult Index()
         {
-            return View();
+            DateTime date = DateTime.Now;
+            return View(date);
         }
     }
 }
