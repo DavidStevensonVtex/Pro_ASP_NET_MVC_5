fead7733acc5f932038150e23a1719d4b3a8bbb0
David Stevenson <drstevenson1958@yahoo.com>
2023-07-21 12:38:12 -0400

    Listing 18-48. Applying Multiple Filters to an Action in the CustomerController.cs File

--- ch18/Filters/Filters/Controllers/CustomerController.cs
+++ ch18/Filters/Filters/Controllers/CustomerController.cs
@@ -1,9 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using Filters.Infrastructure;
+using System.Web.Mvc;
 
 namespace Filters.Controllers
 {
 	public class CustomerController : Controller
     {
+        [SimpleMessage(Message = "A")]
+        [SimpleMessage(Message = "B")]
         public string Index()
         {
             return "This is the Customer controller";
--- ch18/Filters/Filters/Infrastructure/SimpleMessageAttribute.cs
+++ ch18/Filters/Filters/Infrastructure/SimpleMessageAttribute.cs
@@ -12,12 +12,12 @@ namespace Filters.Infrastructure
 		public string Message { get; set; }
 		public void OnActionExecuting(ActionExecutingContext filterContext)
 		{
-			filterContext.HttpContext.Response.Write($"<div>[Before Action: {Message}]");
+			filterContext.HttpContext.Response.Write($"<div>[Before Action: {Message}]</div>");
 		}
 
 		public void OnActionExecuted(ActionExecutedContext filterContext)
 		{
-			filterContext.HttpContext.Response.Write($"<div>[After Action: {Message}]");
+			filterContext.HttpContext.Response.Write($"<div>[After Action: {Message}]</div>");
 		}
 	}
 }
