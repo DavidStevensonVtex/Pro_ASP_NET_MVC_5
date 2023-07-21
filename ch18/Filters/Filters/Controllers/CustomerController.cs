d2f7fd31ac5c0d9e1700952295acfb3bca287e78
David Stevenson <drstevenson1958@yahoo.com>
2023-07-21 12:39:29 -0400

    Liting 18-49. Using the Order Property in a Filter

--- ch18/Filters/Filters/Controllers/CustomerController.cs
+++ ch18/Filters/Filters/Controllers/CustomerController.cs
@@ -5,8 +5,8 @@ namespace Filters.Controllers
 {
 	public class CustomerController : Controller
     {
-        [SimpleMessage(Message = "A")]
-        [SimpleMessage(Message = "B")]
+        [SimpleMessage(Message = "A", Order = 1)]
+        [SimpleMessage(Message = "B", Order = 2)]
         public string Index()
         {
             return "This is the Customer controller";
