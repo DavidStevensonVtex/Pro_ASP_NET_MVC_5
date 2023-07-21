2f40335bd99471e93a7ce18e1cf2af2ed3c7898a
David Stevenson <drstevenson1958@yahoo.com>
2023-07-21 12:05:24 -0400

    Listing 18-40. The Contents of the ActionFilterAttribute.cs File

---
+++ ch18/Filters/Filters/Infrastructure/ProfileAllAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure
+{
+	public class ProfileAllAttribute : ActionFilterAttribute
+	{
+		private Stopwatch timer;
+
+		public override void OnActionExecuting ( ActionExecutingContext filterContext)
+		{
+			timer = Stopwatch.StartNew();
+		}
+
+		public override void OnResultExecuted(ResultExecutedContext filterContext)
+		{
+			timer.Stop();
+
+			filterContext.HttpContext.Response.Write(
+				$"<div>Total elapsed time: {timer.Elapsed.TotalMilliseconds:N3} milliseconds.</div>");
+		}
+	}
+}
