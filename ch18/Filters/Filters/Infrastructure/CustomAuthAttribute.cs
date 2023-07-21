5f6637723407f345b6be8cdba93aec8efd8c5361
David Stevenson <drstevenson1958@yahoo.com>
2023-07-21 08:44:02 -0400

    Listing 18-10. The Contents of the CustomAuthAttribute.cs File

---
+++ ch18/Filters/Filters/Infrastructure/CustomAuthAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure
+{
+	public class CustomAuthAttribute : AuthorizeAttribute
+	{
+		private bool localAllowed;
+
+		public CustomAuthAttriute(bool allowedParam)
+		{
+			localAllowed = allowedParam;
+		}
+
+		protected override bool AuthorizeCore(HttpContextBase httpContext)
+		{
+			if (httpContext.Request.IsLocal)
+			{
+				return localAllowed;
+			}
+			else
+			{
+				return true;
+			}
+		}
+	}
+}
