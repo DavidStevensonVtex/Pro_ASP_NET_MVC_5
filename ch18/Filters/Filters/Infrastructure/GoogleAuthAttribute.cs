595e388ff79eddbc555e1182cc437a64d7b9ae85
David Stevenson <drstevenson1958@yahoo.com>
2023-07-21 09:06:30 -0400

    Listing 18-15. The Contents of the GoogleAuthAttribute.cs File

---
+++ ch18/Filters/Filters/Infrastructure/GoogleAuthAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+
+namespace Filters.Infrastructure
+{
+	public class GoogleAuthAttribute : FilterAttribute, IAuthenticationFilter
+	{
+		public void OnAuthentication(AuthenticationContext filterContext)
+		{
+			// not implemented
+		}
+
+		public void OnAuthenticationChallenge(AuthenticationChallengeContext context)
+		{
+			if (context.Result == null) 
+			{
+				context.Result = new RedirectToRouteResult(
+					new RouteValueDictionary
+					{
+						{ "controller", "GoogleAccount" },
+						{ "action", "Login" },
+						{ "returnUrl", context.HttpContext.Request.RawUrl }
+					});
+			}
+		}
+	}
+}
