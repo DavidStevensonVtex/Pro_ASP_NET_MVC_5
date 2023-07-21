35c9b9d998fb4510e3b053772e93f760f49addda
David Stevenson <drstevenson1958@yahoo.com>
2023-07-21 12:34:01 -0400

    Listing 18-47. The Contents of the SimpleMessageAttribute.cs File

---
+++ ch18/Filters/Filters/Infrastructure/SimpleMessageAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure
+{
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+	public class SimpleMessageAttribute : FilterAttribute, IActionFilter
+	{
+		public string Message { get; set; }
+		public void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			filterContext.HttpContext.Response.Write($"<div>[Before Action: {Message}]");
+		}
+
+		public void OnActionExecuted(ActionExecutedContext filterContext)
+		{
+			filterContext.HttpContext.Response.Write($"<div>[After Action: {Message}]");
+		}
+	}
+}
