58e2f7b66c21d6b69e534ca56cea1b3c0e5925b5
David Stevenson <drstevenson1958@yahoo.com>
2023-07-21 12:27:33 -0400

    Listing 18-46. The Contents of the CustomerController.cs File

---
+++ ch18/Filters/Filters/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Filters.Controllers
+{
+	public class CustomerController : Controller
+    {
+        public string Index()
+        {
+            return "This is the Customer controller";
+        }
+    }
+}
