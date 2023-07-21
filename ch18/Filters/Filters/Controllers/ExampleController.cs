ed516aead3e76377fff0edd32d242f8a96617a4e
David Stevenson <drstevenson1958@yahoo.com>
2023-07-21 08:38:59 -0400

    Listing 18-8. Appying MUltiple Filters in a Controller Class

---
+++ ch18/Filters/Filters/Controllers/ExampleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Controllers
+{
+    [Authorize(Roles = "trader")]
+    public class ExampleController : Controller
+    {
+        [OutputCache(Duration =60)]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
