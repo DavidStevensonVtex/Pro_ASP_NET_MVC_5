5b52140db47e116e646ba9cf93fcc49bed8e6f64
David Stevenson <drstevenson1958@yahoo.com>
2023-07-21 08:33:12 -0400

    Listing 18-6.5. Explicitly Checking Authorization in Action Methods

---
+++ ch18/Filters/Filters/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Filters.Controllers
+{
+	public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (!Request.IsAuthenticated)
+			{
+                FormsAuthentication.RedirectToLoginPage();
+			}
+
+            return null;
+        }
+
+        public ViewResult Create()
+		{
+            if (!Request.IsAuthenticated)
+			{
+                FormsAuthentication.RedirectToLoginPage();
+            }
+
+            return null;
+        }
+    }
+}
