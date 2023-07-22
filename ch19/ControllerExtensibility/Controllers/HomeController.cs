1180eeec951b903fa79e5af0de4b5d2217b6c1c2
David Stevenson <drstevenson1958@yahoo.com>
2023-07-22 10:14:18 -0400

    Listing 19-20. The Contents of the HomeController.cs File

---
+++ ch19/ControllerExtensibility/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using ControllerExtensibility.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Home",
+                ActionName = "Index"
+            });
+        }
+
+        [ActionName("Index")]
+        public ActionResult LocalIndex()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Home",
+                ActionName = "LocalIndex"
+            });
+        }
+        //The current request for action 'Index' on controller type 'HomeController' is ambiguous between the following action methods:
+        //System.Web.Mvc.ActionResult LocalIndex() on type ControllerExtensibility.Controllers.HomeController
+        //System.Web.Mvc.ActionResult Index() on type ControllerExtensibility.Controllers.HomeController
+    }
+}
