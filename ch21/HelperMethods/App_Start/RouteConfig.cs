e00bc839535b8e59c8d88e7ad609354662b00d2d
David Stevenson <drstevenson1958@yahoo.com>
2023-07-23 14:34:54 -0400

    Listing 21-20. Adding a New Route to the RouteConfig.cs File

--- ch21/HelperMethods/App_Start/RouteConfig.cs
+++ ch21/HelperMethods/App_Start/RouteConfig.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace HelperMethods
 {
-    public class RouteConfig
+	public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
@@ -18,6 +14,10 @@ namespace HelperMethods
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "FormRoute",
+                url: "app/forms/{controller}/{action}");
         }
     }
 }
