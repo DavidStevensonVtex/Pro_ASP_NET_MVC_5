571ecf53dec601f45d040cfbf9f522330c5578c6
David Stevenson <drstevenson1958@yahoo.com>
2023-07-23 13:45:25 -0400

    Listing 21-5. The Contents of the CustomHelpers.cs File

---
+++ ch21/HelperMethods/Infrastructure/CustomHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace HelperMethods.Infrastructure
+{
+	public static class CustomHelpers
+	{
+		public static MvcHtmlString ListArrayItems ( this HtmlHelper html, string[] list )
+		{
+			XElement ul = new XElement("ul",
+				list.ToList().Select(li => new XElement("li", li))
+			);
+
+			return new MvcHtmlString(ul.ToString());
+		}
+	}
+}
