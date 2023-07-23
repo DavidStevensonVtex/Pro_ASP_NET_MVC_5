1c82fef27a3fd1c5f80d6c1cccf2654f666518cb
David Stevenson <drstevenson1958@yahoo.com>
2023-07-23 11:54:02 -0400

    Listing 10-12. The Contents of the CustomLocationViewEngine.cs File

--- ch20/WorkingWithRazor/Infrastructure/CustomLocationViewEngine.cs
+++ ch20/WorkingWithRazor/Infrastructure/CustomLocationViewEngine.cs
@@ -9,7 +9,7 @@ namespace WorkingWithRazor.Infrastructure
 			ViewLocationFormats = new string[]
 			{
 				"~/Views/{1}/{0}.cshtml",
-				"~/Views/{1}/{0}.cshtml"
+				"~/Views/Common/{0}.cshtml"
 			};
 		}
 	}
