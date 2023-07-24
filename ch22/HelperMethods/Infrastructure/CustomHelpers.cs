92afe71d9e6aea27f3afc04d5645b81a6879bcc5
David Stevenson <drstevenson1958@yahoo.com>
2023-07-24 06:55:10 -0400

    Chapter 22: Templated Helper Methods

---
+++ ch22/HelperMethods/Infrastructure/CustomHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace HelperMethods.Infrastructure
+{
+	public static class CustomHelpers
+	{
+		public static MvcHtmlString ListArrayItems ( this HtmlHelper html, string[] list )
+		{
+			XElement ul = new XElement("ul",
+				list.ToList().Select(li => new XElement("li", li))
+			);
+
+			return new MvcHtmlString(ul.ToString());
+		}
+
+		public static MvcHtmlString DisplayMessage ( this HtmlHelper html, string msg )
+		{
+			string encodedMessage = html.Encode(msg);
+			return new MvcHtmlString($"This is the message: <p>{encodedMessage}</p>");
+		}
+	}
+}
