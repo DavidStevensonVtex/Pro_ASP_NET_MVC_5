7eed53f04610655d4b44e88157c227d6a96b9c92
David Stevenson <drstevenson1958@yahoo.com>
2023-07-24 09:14:27 -0400

    Listing 22-11. Using the HiddenInput Attribute in the Person.cs File

--- ch22/HelperMethods/Models/Person.cs
+++ ch22/HelperMethods/Models/Person.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Web.Mvc;
 
 namespace HelperMethods.Models
 {
 	public class Person
 	{
+		[HiddenInput]
 		public int PersonId { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
