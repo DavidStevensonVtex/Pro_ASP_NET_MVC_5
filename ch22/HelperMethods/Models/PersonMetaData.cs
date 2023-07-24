f7fd3f4b4d71e5c414a581d4bd15481e51f9b945
David Stevenson <drstevenson1958@yahoo.com>
2023-07-24 09:39:10 -0400

    Listing 22-16. A Partial Model Class in the Person.cs File

--- ch22/HelperMethods/Models/PersonMetaData.cs
+++ ch22/HelperMethods/Models/PersonMetaData.cs
@@ -5,31 +5,27 @@ using System.Web.Mvc;
 
 namespace HelperMethods.Models
 {
-	[MetadataType(typeof(PersonMetaData))]
-	public partial class Person
+	[DisplayName("New Person")]
+	public partial class PersonMetaData
 	{
+		[HiddenInput(DisplayValue = false)]
 		public int PersonId { get; set; }
+
+		[Display(Name = "First")]
+		[UIHint("MultilineText")]
 		public string FirstName { get; set; }
+
+		[Display(Name = "Last")]
 		public string LastName { get; set; }
+
+		[Display(Name = "Birth Date")]
+		[DataType(DataType.Date)]
 		public DateTime BirthDate { get; set; }
+
 		public Address HomeAddress { get; set; }
+
+		[Display(Name = "Approved")]
 		public bool IsApproved { get; set; }
 		public Role Role { get; set; }
-	}
-
-	public class Address
-	{
-		public string Line1 { get; set; }
-		public string Line2 { get; set; }
-		public string City { get; set; }
-		public string PostalCode { get; set; }
-		public string Country { get; set; }
-	}
-
-	public enum Role
-	{
-		Admin,
-		User,
-		Guest
 	}
 }
