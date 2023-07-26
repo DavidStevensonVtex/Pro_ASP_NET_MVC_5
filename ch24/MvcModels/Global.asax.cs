dfa08424737388e2abda16a8247463d58ca3616d
David Stevenson <drstevenson1958@yahoo.com>
2023-07-26 13:03:06 -0400

    Listing 24-36. Registering a Value Provider Factory in the Global.asax.cs File

--- ch24/MvcModels/Global.asax.cs
+++ ch24/MvcModels/Global.asax.cs
@@ -1,18 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using MvcModels.Infrastructure;
 
 namespace MvcModels
 {
-    public class MvcApplication : System.Web.HttpApplication
+	public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ValueProviderFactories.Factories.Insert(0, new CustomValueProviderFactory());
         }
     }
 }
