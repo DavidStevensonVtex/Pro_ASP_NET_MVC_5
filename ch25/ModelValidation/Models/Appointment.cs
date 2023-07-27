ff25229706f1b7797ff724d984386d2a1e66922a
David Stevenson <drstevenson1958@yahoo.com>
2023-07-27 15:27:22 -0400

    Defining Self-Validating Models
    Listing 25-20. Adding Self-Validatin to the Appointment.cs File

--- ch25/ModelValidation/Models/Appointment.cs
+++ ch25/ModelValidation/Models/Appointment.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using ModelValidation.Infrastructure;
 
 namespace ModelValidation.Models
 {
 	[NoJoeOnMondays]
-	public class Appointment
+	public class Appointment : IValidatableObject
 	{
 		[Required(ErrorMessage = "Please enter your name")]
 		public string ClientName { get; set; }
@@ -16,5 +17,33 @@ namespace ModelValidation.Models
 
 		[MustBeTrue(ErrorMessage = "You must accept the terms")]
 		public bool TermsAccepted { get; set; }
+
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			List<ValidationResult> errors = new List<ValidationResult>();
+
+			if (string.IsNullOrEmpty(ClientName))
+			{
+				errors.Add(new ValidationResult("Please enter your name"));
+			}
+
+			if (DateTime.Now > Date)
+			{
+				errors.Add(new ValidationResult("Please enter a date in the future."));
+			}
+
+			// Joe cannot make Appointments on Monday
+			if (errors.Count == 0 && ClientName == "Joe" && Date.DayOfWeek == DayOfWeek.Monday)
+			{
+				errors.Add(new ValidationResult("Joe cannot book appointments on Mondays"));
+			}
+
+			if (!TermsAccepted)
+			{
+				errors.Add(new ValidationResult("You must accept the terms"));
+			}
+
+			return errors;
+		}
 	}
 }
