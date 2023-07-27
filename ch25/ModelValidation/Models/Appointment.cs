66c63fd10294969e717792b306f1073c3f291ed0
David Stevenson <drstevenson1958@yahoo.com>
2023-07-27 15:48:23 -0400

    Listing 25-24. Adding a Validation Action Method to the HomeController.cs File

--- ch25/ModelValidation/Models/Appointment.cs
+++ ch25/ModelValidation/Models/Appointment.cs
@@ -6,44 +6,15 @@ using ModelValidation.Infrastructure;
 namespace ModelValidation.Models
 {
 	[NoJoeOnMondays]
-	public class Appointment : IValidatableObject
+	public class Appointment
 	{
-		[Required(ErrorMessage = "Please enter your name")]
+		[Required]
+		[StringLength(10, MinimumLength = 3)]
 		public string ClientName { get; set; }
 
-		[FutureDate(ErrorMessage = "Please enter a date in the future")]
 		[DataType(DataType.Date)]
 		public DateTime Date { get; set; }
 
-		[MustBeTrue(ErrorMessage = "You must accept the terms")]
 		public bool TermsAccepted { get; set; }
-
-		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-		{
-			List<ValidationResult> errors = new List<ValidationResult>();
-
-			if (string.IsNullOrEmpty(ClientName))
-			{
-				errors.Add(new ValidationResult("Please enter your name"));
-			}
-
-			if (DateTime.Now > Date)
-			{
-				errors.Add(new ValidationResult("Please enter a date in the future."));
-			}
-
-			// Joe cannot make Appointments on Monday
-			if (errors.Count == 0 && ClientName == "Joe" && Date.DayOfWeek == DayOfWeek.Monday)
-			{
-				errors.Add(new ValidationResult("Joe cannot book appointments on Mondays"));
-			}
-
-			if (!TermsAccepted)
-			{
-				errors.Add(new ValidationResult("You must accept the terms"));
-			}
-
-			return errors;
-		}
 	}
 }
