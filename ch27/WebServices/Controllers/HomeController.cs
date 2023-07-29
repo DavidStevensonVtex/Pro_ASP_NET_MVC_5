78806ddae5e11887c0f363eb4075546ceadd0fef
David Stevenson <drstevenson1958@yahoo.com>
2023-07-29 16:21:05 -0400

    Listing 27-5. The Contents of the Index.cshtml File

--- ch27/WebServices/Controllers/HomeController.cs
+++ ch27/WebServices/Controllers/HomeController.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using WebServices.Models;
 
 namespace WebServices.Controllers
 {
-    public class HomeController : Controller
+	public class HomeController : Controller
     {
         private ReservationRepository repo = ReservationRepository.Current;
         public ActionResult Index()
