32efbffbb6712df3bd20823e15ba840890e81aeb
David Stevenson <drstevenson1958@yahoo.com>
2023-07-29 20:17:20 -0400

    Listing 27-16. Removing the Data Selection from the HomeController.cs File

--- ch27/WebServices/Controllers/HomeController.cs
+++ ch27/WebServices/Controllers/HomeController.cs
@@ -1,45 +1,12 @@
 ﻿using System.Web.Mvc;
-using WebServices.Models;
 
 namespace WebServices.Controllers
 {
 	public class HomeController : Controller
     {
-        private ReservationRepository repo = ReservationRepository.Current;
         public ActionResult Index()
         {
-            return View(repo.GetAll());
+            return View();
         }
-
-        public ActionResult Add(Reservation item)
-		{
-            if (ModelState.IsValid)
-			{
-                repo.Add(item);
-                return RedirectToAction("Index");
-			}
-            else
-			{
-                return View("Index");
-			}
-		}
-
-        public ActionResult Remove(int id)
-		{
-            repo.Remove(id);
-            return RedirectToAction("Index");
-		}
-
-        public ActionResult Update( Reservation item)
-		{
-            if (ModelState.IsValid && repo.Update(item))
-			{
-                return RedirectToAction("Index");
-			}
-            else
-			{
-                return View("Index");
-			}
-		}
     }
 }
